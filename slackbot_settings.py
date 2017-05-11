78db6331e2ca0e12e0c905ea7587fa011deaa8b8
TheESOKRoshan <rk3110@nyu.edu>
2017-05-11 02:05:26 -0400

    Adding script to create the bot

---
+++ slackbot_settings.py
@@ -0,0 +1,6 @@
+API_TOKEN = "xoxb-147194780839-ejiT59mSKx9WCqflZSLYdXzG"
+DEFAULT_REPLY = "Thanks officer you made my day!"
+ERRORS_TO = 'shivendranitb'
+PLUGINS = [
+            'slackbot.plugins',
+        ]
